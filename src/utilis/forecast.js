0989ce10d46e2709b435b31857ac3d89df7fa1fb
Your Name <you@example.com>
2023-02-22 18:16:25 +0530

    Added the wind speed

--- src/utilis/forecast.js
+++ src/utilis/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longtitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out And  the wind speed is ${body.current.wind_speed}`
       );
     }
   });
